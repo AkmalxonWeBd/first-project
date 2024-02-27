82a8cb510922cd8d1066bea23c6bc82e39a26463
mikecoderweb <muydinkhon.abduvaliev04@gmail.com>
2024-02-27 15:49:53 +0500

    salommm

--- src/components/redux/store/index.js
+++ src/components/redux/store/index.js
@@ -2,22 +2,21 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { ProductCrud } from '../slice/product';
 import { OrderCrud } from '../slice/order/order';
-import { SubcategoriesCrud } from '../slice/SubCategories/crud';
-import { categoriesApi } from '../slice/CategoriesCrud/crud'; // Import named export
-
+import { categoriesApi } from '../slice/CategoriesCrud/crud'; 
+import { SubCategoryCrud } from '../slice/client/subcategory';
 export const store = configureStore({
   reducer: {
     [ProductCrud.reducerPath]: ProductCrud.reducer,
     [OrderCrud.reducerPath]: OrderCrud.reducer,
-    [SubcategoriesCrud.reducerPath]: SubcategoriesCrud.reducer,
-    [categoriesApi.reducerPath]: categoriesApi.reducer, // Use named export
+    [SubCategoryCrud.reducerPath]: SubCategoryCrud.reducer,
+    [categoriesApi.reducerPath]: categoriesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       categoriesApi.middleware,
       ProductCrud.middleware,
       OrderCrud.middleware,
-      SubcategoriesCrud.middleware
+      SubCategoryCrud.middleware
     ),
 });
 
