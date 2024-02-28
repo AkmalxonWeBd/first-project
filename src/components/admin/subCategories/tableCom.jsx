2c90f1a16ef6a0b844cf4e79ebdfd31fb4a878fd
55zakii <zodilbek2009@gmail.com>
2024-02-28 17:36:47 +0500

    20-may

--- src/components/admin/subCategories/tableCom.jsx
+++ src/components/admin/subCategories/tableCom.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import { useGetSubcatigoriesQuery } from "../../redux/slice/SubCategories/crud";
-import AddCategoriesCom from "./AddCategiresCom";
+import { useGetCategoriesQuery } from "../../redux/slice/CategoriesCrud/crud";
+import AddSubCategories from "./AddCategiresCom";
+import SubUpdateCategories from "./UpdateCategoriesCom";
+import DeleteCategories from "./DeleteCategoriesCom";
 
 const TableCom = () => {
-  const { data: categories, error, isLoading } = useGetSubcatigoriesQuery();
+  const { data, error, isLoading } = useGetCategoriesQuery();
+  const [search, setSearch] = useState("");
 
-  console.log(categories, 'categories');
-  const [, setSearch] = useState("");
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.error}</div>;
 
+  const categories = data || [];
+
+  const filteredData = categories.filter((item) =>
+    item.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div class=" pt-10px relative overflow-x-auto bg-red-500 h-[99vh] shadow-md sm:rounded-lg">
-      <div className="flex  justify-between">
+    <div className="bg-white-800 dark:bg-white-900 p-3 sm:p-4 antialiased">
+      <div className="flex justify-between">
         <div>
           <input
             onChange={(e) => setSearch(e.target.value)}
             type="text"
             id="Oziq Ovqadlar"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Search....."
             required
           />
@@ -26,45 +33,48 @@ const TableCom = () => {
         <div>
           <button
             type="button"
-            class="inline-flex items-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="inline-flex items-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm"
           >
-            {" "}
-            <AddCategoriesCom/>{" "}
+            <AddSubCategories/>
           </button>
         </div>
       </div>
-      <table class="w-full  overflow-y-scroll  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" class="p-4">
-              Mahsulod turi rasmi
-            </th>
-            <th scope="col" class="p-4">
-              Mahsulod turi nomi
-            </th>
-            <th scope="col" class="p-4">action</th>
-          </tr>
-
-          {[...categories].reverse()?.map((value) => {
-            return (
-              <tr>
-                <th scope="col" class="p-4">
-                  <img className="rounded-2xl  w-[70px] h-[60px]" src={value?.image} alt={value?.title} />
-                </th>
-                <th scope="col" class="p-4">
-                  {value?.title}
-                </th>
-                <th scope="col" class="p-4 flex gap-2">
-                  <button>edit</button>
-                  <button>delte</button>
-                </th>
-
+      <div className="overflow-x-auto h-[80vh]">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 w-full">
+            <tr>
+              <th scope="col" className="p-4">
+              SUBKATEGORIYA RASMI
+              </th>
+              <th scope="col" className="p-4">
+              SUBKATEGORIYA NOMI
+              </th>
+              <th scope="col" className="p-4">
+              KATTA KATEGORY NOMI
+              </th>
+              <th scope="col" className="p-4"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.reverse().map((value) => (
+              <tr key={value.id}>
+                <td className="p-4">
+                  <img
+                    className="rounded-2xl w-[70px] h-[60px]"
+                    src={value.image}
+                    alt={value.title}
+                  />
+                </td>
+                <td className="p-4">{value.title}</td>
+                <td className="p-4 flex gap-2">
+                  <SubUpdateCategories/>
+                  <DeleteCategories/>
+                </td>
               </tr>
-            )
-          })}
-        </thead>
-        <tbody></tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
