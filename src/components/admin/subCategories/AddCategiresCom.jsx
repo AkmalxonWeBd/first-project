d4f9bbf86b65e13bc33c54ba448b20d2c2d9b59a
55zakii <zodilbek2009@gmail.com>
2024-02-28 17:39:00 +0500

    d

--- src/components/admin/subCategories/AddCategiresCom.jsx
+++ src/components/admin/subCategories/AddCategiresCom.jsx
@@ -5,6 +5,7 @@ import { useCreateSubCatigoriesMutation } from "../../redux/slice/SubCategories/
 import ImageUpload from "../../generic/imgUpload";
 import Modal from "../../generic/modal";
 
+
 const AddCategoriesCom = () => {
   const [open, setOpen] = useState(false);
 
--- src/components/generic/modal.jsx
+++ src/components/generic/modal.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ButtonLoader from "./buttonLoader";
-// import ButtonLoader from "../Loader/ButtonLoader";
+///
+/////
+/////
+
 export default function Modal({
   title,
   addFunc,
