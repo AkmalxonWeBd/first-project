5537dcaa1ad22846acf1b8ec20b5f4aa0d63906c
mikecoderweb <muydinkhon.abduvaliev04@gmail.com>
2024-03-05 15:18:34 +0500

    commt

--- src/components/client/Product/index.jsx
+++ src/components/client/Product/index.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useGetProductQuery } from "../../redux/slice/product";
 
+const Skeleton = () => (
+  <div className="animate-pulse w-48 h-48 bg-gray-300 rounded-lg"></div>
+);
+
 const Product = () => {
   const { data, isLoading } = useGetProductQuery();
   const [startIndex, setStartIndex] = useState(0);
@@ -10,43 +14,50 @@ const Product = () => {
   const showMore = () => {
     setStartIndex((prevIndex) => prevIndex + productsToShow);
   };
-<h1></h1>
+
   const showLess = () => {
     setStartIndex(0);
   };
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {data?.slice(0, startIndex + productsToShow).map((product, index) => (
-        <div
-          key={index}
-          className="relative"
-          onMouseEnter={() => setHoveredProductindex(index)}
-          onMouseLeave={() => setHoveredProductindex(null)}
-        >
-          <div className="w-48 h-full rounded-lg shadow-lg overflow-hidden relative">
-            <img
-              className="w-full h-full object-cover"
-              src={product.image}
-              alt=""
-            />
-            {hoveredProductindex === index && (
-              <button
-                onClick={() =>
-                  console.log("Add to Basket clicked for", product.name)
-                }
-                className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-              >
-                Add to Basket
-              </button>
-            )}
+    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
+      {isLoading ? (
+        Array.from({ length: productsToShow }).map((_, index) => (
+          <Skeleton key={index} />
+        ))
+      ) : (
+        data?.slice(0, startIndex + productsToShow).map((product, index) => (
+          <div
+            key={index}
+            className="relative"
+            onMouseEnter={() => setHoveredProductindex(index)}
+            onMouseLeave={() => setHoveredProductindex(null)}
+          >
+            <div className="w-48 h-full rounded-lg shadow-lg overflow-hidden relative">
+              <img
+                className="w-full h-full object-cover"
+                src={product.image}
+                alt=""
+              />
+              {hoveredProductindex === index && (
+                <button
+                  onClick={() =>
+                    console.log("Add to Basket clicked for", product.name)
+                  }
+                  className="absolute bottom-4 left-11 bg-blue-500 text-white px-2 py-1 rounded-lg shadow-lg"
+
+                >
+                  Add to Basket
+                </button>
+              )}
+            </div>
+            <div className="mt-2 text-center">
+              <p className="text-lg font-bold">{product.name}</p>
+              <p className="text-gray-600">{product.price}</p>
+            </div>
           </div>
-          <div className="mt-2 text-center">
-            <p className="text-lg font-bold">{product.name}</p>
-            <p className="text-gray-600">{product.price}</p>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
       {startIndex + productsToShow >= data?.length && (
         <div className="col-span-4 flex justify-center">
           <button
@@ -58,20 +69,22 @@ const Product = () => {
         </div>
       )}
       {startIndex + productsToShow < data?.length && (
-        <div className="col-span-4 flex justify-center">
-          <button
-            onClick={showMore}
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-          >
-            View More
-          </button>
-        </div>
+       <div className={`col-span-4 md:grid-cols-4 sm:col-span-2 flex ${startIndex === 0 ? 'justify-center' : 'justify-end'} items-center`}>
+       <button
+         onClick={showMore}
+         className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
+       >
+         View More
+       </button>
+     </div>     
       )}
     </div>
   );
 };
 
 export default Product;
+
+
 
 
 // import React, { useState } from "react";
