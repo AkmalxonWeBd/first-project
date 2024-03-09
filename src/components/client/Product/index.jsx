79a200c8be20decf0e603cca3457651e0f647df6
mikecoderweb <muydinkhon.abduvaliev04@gmail.com>
2024-03-09 16:12:29 +0500

    commeet

--- src/components/client/Product/index.jsx
+++ src/components/client/Product/index.jsx
@@ -11,7 +11,7 @@ const Product = () => {
   const { data, isLoading } = useGetProductQuery();
   const [startIndex, setStartIndex] = useState(0);
   const [hoveredProductindex, setHoveredProductindex] = useState(null);
- const [createBasket ] =   useCreateBasketMutation()
+  const [createBasket] = useCreateBasketMutation();
   const productsToShow = 4;
 
   const showMore = () => {
@@ -21,57 +21,110 @@ const Product = () => {
   const showLess = () => {
     setStartIndex(0);
   };
- const productBy  =async  (id) => {
 
-  console.log(id,'id');
-  const formData = new FormData();
-  formData.append("amount", 1);
-  formData.append("product", id);
-  try {
-    await createBasket(formData).unwrap();
-    toast.success(`maxsulod  qushildi`);
-  } catch (error) {
-    toast.error(` Mahsulot Qushilmadi `);
-  }
-  // refetch();
-}
+  const [productQuantities, setProductQuantities] = useState({});
+
+  const incrementAmount = (productId) => {
+    setProductQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: (prevQuantities[productId] || 0) + 1,
+    }));
+  };
+
+  const decrementAmount = (productId) => {
+    if (productQuantities[productId] > 1) {
+      setProductQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [productId]: prevQuantities[productId] - 1,
+      }));
+    }
+  };
+
+  const productBy = async (id) => {
+    setProductQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [id]: (prevQuantities[id] || 0) + 1,
+    }));
+  };
+
+  const addToBasket = async (id, amount) => {
+    const formData = new FormData();
+    formData.append("amount", amount);
+    formData.append("product", id);
+    try {
+      await createBasket(formData).unwrap();
+      toast.success(`maxsulod  qushildi`);
+      setProductQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [id]: 1,
+      }));
+    } catch (error) {
+      toast.error(` Mahsulot Qushilmadi `);
+    }
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
-      {isLoading ? (
-        Array.from({ length: productsToShow }).map((_, index) => (
-          <Skeleton key={index} />
-        ))
-      ) : (
-        data?.slice(0, startIndex + productsToShow).map((product, index) => (
-          <div
-            key={index}
-            className="relative"
-            onMouseEnter={() => setHoveredProductindex(index)}
-            onMouseLeave={() => setHoveredProductindex(null)}
-          >
-            <div className="w-48 h-full rounded-lg shadow-lg overflow-hidden relative">
-              <img
-                className="w-full h-full object-cover"
-                src={product.image}
-                alt=""
-              />
-              {hoveredProductindex === index && (
-                <button
-                  onClick={() => productBy(product.id)}
-                  className="absolute bottom-4 left-11 bg-blue-500 text-white px-2 py-1 rounded-lg shadow-lg"
-
-                >
-                  Add to Basket
-                </button>
-              )}
+    <div className="grid grid-cols-2 gap-10 sm:grid-cols-2 md:grid-cols-4">
+      {isLoading
+        ? Array.from({ length: productsToShow * 2 }).map((_, index) => (
+            <Skeleton key={index} />
+          ))
+        : data?.slice(0, startIndex + 8).map((product, index) => (
+            <div
+              key={index}
+              className="relative"
+              onMouseEnter={() => setHoveredProductindex(index)}
+              onMouseLeave={() => setHoveredProductindex(null)}
+            >
+              <div className="w-48 h-full rounded-lg shadow-lg overflow-hidden relative">
+                <img
+                  className="w-full h-full rounded-md"
+                  src={product.image}
+                  alt=""
+                />
+                {(hoveredProductindex === index ||
+                  productQuantities[product.id]) && (
+                  <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center">
+                    {productQuantities[product.id] ? (
+                      <>
+                        <span
+                          onClick={() => decrementAmount(product.id)}
+                          className="cursor-pointer rounded-l bg-blue-500 py-1 px-3.5 text-white duration-100 hover:bg-blue-500 hover:text-blue-50"
+                        >
+                          {" "}
+                          -{" "}
+                        </span>
+                        <input
+                          className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                          type="text"
+                          value={productQuantities[product.id]}
+                          min="1"
+                        />
+                        <span
+                          onClick={() => incrementAmount(product.id)}
+                          className="cursor-pointer rounded-r bg-blue-500 text-white py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                        >
+                          {" "}
+                          +{" "}
+                        </span>
+                      </>
+                    ) : (
+                      <button
+                        onClick={() => productBy(product.id)}
+                        className="bg-black text-white px-9 py-1 shadow-lg"
+                      >
+                        Add to Basket
+                      </button>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="mt-2 text-center">
+                <p className="text-lg font-bold">{product.name}</p>
+                <p className="text-gray-600">{product.price}</p>
+              </div>
             </div>
-            <div className="mt-2 text-center">
-              <p className="text-lg font-bold">{product.name}</p>
-              <p className="text-gray-600">{product.price}</p>
-            </div>
-          </div>
-        ))
-      )}
+          ))}
       {startIndex + productsToShow >= data?.length && (
         <div className="col-span-4 flex justify-center">
           <button
@@ -83,14 +136,18 @@ const Product = () => {
         </div>
       )}
       {startIndex + productsToShow < data?.length && (
-       <div className={`col-span-4 md:grid-cols-4 sm:col-span-2 flex ${startIndex === 0 ? 'justify-center' : 'justify-end'} items-center`}>
-       <button
-         onClick={showMore}
-         className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-       >
-         View More
-       </button>
-     </div>     
+        <div
+          className={`col-span-4 md:grid-cols-4 sm:col-span-2 flex ${
+            startIndex === 0 ? " justify-end" : "justify-end"
+          } items-center`}
+        >
+          <button
+            onClick={showMore}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
+          >
+            View More
+          </button>
+        </div>
       )}
     </div>
   );
@@ -98,80 +155,3 @@ const Product = () => {
 
 export default Product;
 
-
-
-
-// import React, { useState } from "react";
-// import { useGetProductQuery } from "../../redux/slice/product";
-
-// const Product = () => {
-//   const { data, isLoading } = useGetProductQuery();
-//   const [startIndex, setStartIndex] = useState(0);
-//   const [hoveredProductindex, setHoveredProductindex] = useState(null);
-//   const productsToShow = 4;
-
-//   const showMore = () => {
-//     setStartIndex((prevIndex) => prevIndex + productsToShow);
-//   };
-
-//   const showLess = () => {
-//     setStartIndex(0);
-//   };
-
-//   return (
-//     <div className="grid grid-cols-4 gap-4">
-//       {data?.slice(0, startIndex + productsToShow).map((product, index) => (
-//         <div
-//           key={index}
-//           className="flex flex-col items-center"
-//           onMouseEnter={() => setHoveredProductindex(index)}
-//           onMouseLeave={() => setHoveredProductindex(null)}
-//         >
-//           <div className="w-48 h-full rounded-lg shadow-lg overflow-hidden">
-//             <img
-//               className="w-full h-full object-cover"
-//               src={product.image}
-//               alt=""
-//             />
-//             {hoveredProductindex === index && (
-//               <button
-//                 onClick={() =>
-//                   console.log("Add to Basket clicked for", product.name)
-//                 }
-//                 className="absolute bottom-4 left-4 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-//               >
-//                 Add to Basket
-//               </button>
-//             )}
-//           </div>
-//           <div className="mt-2 text-center">
-//             <p className="text-lg font-bold">{product.name}</p>
-//             <p className="text-gray-600">{product.price}</p>
-//           </div>
-//         </div>
-//       ))}
-//       {startIndex + productsToShow >= data?.length && (
-//         <div className="col-span-4 flex justify-center">
-//           <button
-//             onClick={showLess}
-//             className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-//           >
-//             View Less
-//           </button>
-//         </div>
-//       )}
-//       {startIndex + productsToShow < data?.length && (
-//         <div className="col-span-4 flex justify-center">
-//           <button
-//             onClick={showMore}
-//             className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg"
-//           >
-//             View More
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Product;
