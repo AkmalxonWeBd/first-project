48e127fce3f2371f95fa50e7afda787f38b30220
AkmalxonWeBd <akmalxonizzatullayev770@gmail.com>
2024-03-09 16:08:16 +0500

    salom

--- src/components/client/navbar/navbar.jsx
+++ src/components/client/navbar/navbar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {  NavLink, useLocation } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import { FaRegHeart } from "react-icons/fa";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
+import "./navbar.css"
 const Navbar = () => {
+  const location = useLocation()
   return (
     <div className="  flex h-[70px] items-center  justify-around px-3 w-full pt-[30px]">
       <h1 className="font-bold text-2xl">Exclusive</h1>
       <ul className="flex justify-around gap-12  ">
-        <Link to={"/"}><li>Home</li>{" "}</Link>
-        <Link to={"/contact"}><li>Contact</li></Link>
-        <Link to={"/shop"}><li>Shop</li></Link>
-        <Link to={"/signUp"}><li>Sign Up</li></Link>
+        <NavLink className={`span , span1 ${location.pathname === '/' && 'active'}`} to="/"><li>Home</li></NavLink>
+        <NavLink className={`span , span2 ${location.pathname === '/contact' && 'active'}`} to="/contact"><li>Contact</li></NavLink>
+        <NavLink className={`span , span3 ${location.pathname === '/shop' && 'active'}`} to="/shop"><li>Shop</li></NavLink>
+        <NavLink className={`span , span4 ${location.pathname === '/signUp' && 'active'}`} to="/signUp"><li>Sign Up</li></NavLink>
       </ul>
       <div className=" flex justify-between w-[350px] items-center">
         <div className=" flex  items-center">
           <input type="search" placeholder="What are you looking for?" className="w-[240px] h-[40px] pl-2  bg-yellow-100	" />
           <IoSearch className="font-bold text-2xl " />
         </div>
-        <FaRegHeart className="font-bold text-2xl "/>
-        <HiOutlineShoppingCart className="font-bold text-2xl "/>
+        <FaRegHeart className="font-bold text-2xl " />
+        <HiOutlineShoppingCart className="font-bold text-2xl " />
       </div>
     </div>
   );
